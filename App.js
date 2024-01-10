0d78bbddd7a14c7f291cc5ef50da6ed10c4b34db
Samaar Ahmed <diamantbilvard@gmail.com>
2024-01-10 17:35:07 +0100

    Add app envirement

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Welcome to Guessing Game</Text>
       <StatusBar style="auto" />
     </View>
   );
